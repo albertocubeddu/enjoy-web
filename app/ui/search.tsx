c7d969c590b85d5fb5438f43192adeeac463e40a
Sam Burke <74158978+samburke97@users.noreply.github.com>
2024-03-16 14:30:06 +0000

    Search bar data fix

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,22 +5,22 @@ import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
+  // const searchParams = useSearchParams();
+  // const pathname = usePathname();
+  // const { replace } = useRouter();
 
-  const handleSearch = useDebouncedCallback((term: string) => {
-    console.log(`Searching... ${term}`);
+  // const handleSearch = useDebouncedCallback((term: string) => {
+  //   console.log(`Searching... ${term}`);
 
-    const params = new URLSearchParams(searchParams);
-    params.set("page", "1");
+  //   const params = new URLSearchParams(searchParams);
+  //   params.set("page", "1");
 
-    if (term) {
-      params.set("query", term);
-    } else {
-      params.delete("query");
-    }
-  }, 300);
+  //   if (term) {
+  //     params.set("query", term);
+  //   } else {
+  //     params.delete("query");
+  //   }
+  // }, 300);
 
   return (
     <form className="relative flex flex-1 flex-shrink-0 gap-x-4">
@@ -30,10 +30,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500  "
         placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get("query")?.toString()}
+        // onChange={(e) => {
+        //   handleSearch(e.target.value);
+        // }}
+        // defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
       <button className="text-white text-sm text-nowrap border-1 bg-primary-primary px-8 py-1 rounded-xl">
