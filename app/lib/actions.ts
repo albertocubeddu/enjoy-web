c67d90d242069783940574fe4ff7c7a1438cd6e0
Sam Burke <74158978+samburke97@users.noreply.github.com>
2024-03-26 00:27:57 +0000

    Edited actions.ts for Reddit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,23 @@
 "use server";
 
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+
+const FormSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  image: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true });
+
 export async function createSport(formData: FormData) {
-  const rawFormData = {
+  const { name, image } = CreateInvoice.parse({
     name: formData.get("name"),
     image: formData.get("image") as Blob,
-  };
-
-  console.log(rawFormData);
+  });
+  await sql`
+    INSERT INTO sports (name, image_url)
+    VALUES (${name}, ${image})
+  `;
 }
